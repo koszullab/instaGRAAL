f59d624316a27a181944befae7917d47482c661a
Nadège Guiglielmoni <nguiglielmoni@wanadoo.fr>
2024-03-20 17:55:24 +0100

    Update kernel_sparse.cu

--- instagraal/kernels/kernel_sparse.cu
+++ instagraal/kernels/kernel_sparse.cu
@@ -2944,12 +2944,21 @@ extern "C"
     }
 
 
-    __device__ inline double __shfl_down(double var, unsigned int srcLane, int width=32) {
-        int2 a = *reinterpret_cast<int2*>(&var);
-        a.x = __shfl_down_sync(a.x, srcLane, width);
-        a.y = __shfl_down_sync(a.y, srcLane, width);
-        return *reinterpret_cast<double*>(&a);
-    }
+#if !defined(__CUDA_ARCH__) || __CUDA_ARCH__ >= 300
+__device__ inline double __shfl_down(double var, unsigned int srcLane, int width=32) {
+   int2 a = *reinterpret_cast<int2*>(&var);
+   a.x = __shfl_down_sync(a.x, srcLane, width);
+   a.y = __shfl_down_sync(a.y, srcLane, width);
+   return *reinterpret_cast<double*>(&a);
+}
+#else
+__device__ inline double __shfl_down(double var, unsigned int srcLane, int width=32) {
+   int2 a = *reinterpret_cast<int2*>(&var);
+   a.x = __shfl_down(a.x, srcLane, width);
+   a.y = __shfl_down(a.y, srcLane, width);
+   return *reinterpret_cast<double*>(&a);
+}
+#endif
 
     __inline__ __device__ double warpReduceSum(double val) {
         for (int offset = warpSize/2; offset > 0; offset /= 2)
